81a6479060cb23655bc362bb866481e0ec00508d
bluesk1 <sraghukannan@gmail.com>
2023-03-01 19:07:19 -0600

    Offline enabled

--- service-worker.js
+++ service-worker.js
@@ -1,104 +1,52 @@
-/*
-Copyright 2015, 2019, 2020, 2021 Google LLC. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
+// List the files to cache
+const CACHE_NAME = 'my-cache-v1';
+const urlsToCache = [
+  'index.html',
+  'index.css',
+  'index.js',
+  'jquery.min.js',
+  'p5.js',
+  'p5.sound.min.js',
+  'qrcode.js',
+  'ClickableObject.js',
+  'ChargeStationButton.js',
+  'StartButton.js',
+  'StartTeleOpButton.js',
+  'FinishedScreen.js',
+  'Clock.js',
+  'FieldImage.png',
+  'carpet.jfif',
+  'high.png',
+  'mid.png',
+  'docked.png',
+  'engaged.png',
+  'ground.png',
+  'scout.html',
+  'sketch.js',
+  'sketch2.js',
+  'daisyui.js',
+  'daisyui.css',
+  'jsoncrush.min.js',
+  'service-worker.js'
+];
 
-// Incrementing OFFLINE_VERSION will kick off the install event and force
-// previously cached resources to be updated from the network.
-// This variable is intentionally declared and unused.
-// Add a comment for your linter if you want:
-// eslint-disable-next-line no-unused-vars
-const OFFLINE_VERSION = 1;
-const CACHE_NAME = "offline";
-// Customize this with a different URL if needed.
-// ChargeStationButton.js	carpet.jfif		jsoncrush.min.js
-// ClickableObject.js	docked.png		mid.png
-// Clock.js		engaged.png		p5.js
-// FieldImage.png		ground.png		p5.sound.min.js
-// FinishedScreen.js	high.png		qrcode.js
-// Node.js			index.css		scout.html
-// ServiceWorker.js	index.html		sketch.js
-// StartButton.js		index.js		sketch2.js
-// StartTeleOpButton.js	jquery.min.js
-const STATIC_FILES = ['index.html', 'index.css', 'index.js', 'jsoncrush.min.js', 'p5.js', 'p5.sound.min.js', 'qrcode.js', 'jquery.min.js', 'sketch.js', 'sketch2.js', 'carpet.jfif', 'docked.png', 'engaged.png', 'ground.png', 'mid.png', 'high.png', 'FieldImage.png', 'ClickableObject.js', 'Node.js', 'StartButton.js', 'StartTeleOpButton.js', 'Clock.js', 'ChargeStationButton.js', 'FinishedScreen.js', 'ServiceWorker.js', 'daisyui.js', 'daisyui.css'];
-
-self.addEventListener("install", (event) => {
+// Install the Service Worker and cache files
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    (async () => {
-      const cache = await caches.open(CACHE_NAME);
-      // Setting {cache: 'reload'} in the new request ensures that the
-      // response isn't fulfilled from the HTTP cache; i.e., it will be
-      // from the network.
-    //   await cache.add(new Request(OFFLINE_URL, { cache: "reload" }));
-        // add it one  by one 
-        for (let i = 0; i < STATIC_FILES.length; i++) {
-            await cache.add(STATIC_FILES[i]);
-        }
-    })()
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
   );
-  // Force the waiting service worker to become the active service worker.
-  self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    (async () => {
-      // Enable navigation preload if it's supported.
-      // See https://developers.google.com/web/updates/2017/02/navigation-preload
-      if ("navigationPreload" in self.registration) {
-        await self.registration.navigationPreload.enable();
-      }
-    })()
+// Fetch cached files when offline
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Return cached response if found, otherwise fetch from network
+        return response || fetch(event.request);
+      })
   );
-
-  // Tell the active service worker to take control of the page immediately.
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
-  // Only call event.respondWith() if this is a navigation request
-  // for an HTML page.
-  if (event.request.mode === "navigate") {
-    event.respondWith(
-      (async () => {
-        try {
-          // First, try to use the navigation preload response if it's
-          // supported.
-          const preloadResponse = await event.preloadResponse;
-          if (preloadResponse) {
-            return preloadResponse;
-          }
-
-          // Always try the network first.
-          const networkResponse = await fetch(event.request);
-          return networkResponse;
-        } catch (error) {
-          // catch is only triggered if an exception is thrown, which is
-          // likely due to a network error.
-          // If fetch() returns a valid HTTP response with a response code in
-          // the 4xx or 5xx range, the catch() will NOT be called.
-          console.log("Fetch failed; returning offline page instead.", error);
-
-          const cache = await caches.open(CACHE_NAME);
-          const cachedResponse = await cache.match(OFFLINE_URL);
-          return cachedResponse;
-        }
-      })()
-    );
-  }
-
-  // If our if() condition is false, then this fetch handler won't
-  // intercept the request. If there are any other fetch handlers
-  // registered, they will get a chance to call event.respondWith().
-  // If no fetch handlers call event.respondWith(), the request
-  // will be handled by the browser as if there were no service
-  // worker involvement.
 });
